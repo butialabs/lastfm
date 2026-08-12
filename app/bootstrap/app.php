c8ad10e3c104388813c5bbd48a961991ab39a6d8
Renan Bernordi <altendorfme@gmail.com>
2026-08-12 13:08:20 -0300

    add sentry support

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             SetLocale::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {})
+    ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+    })
     ->create();
